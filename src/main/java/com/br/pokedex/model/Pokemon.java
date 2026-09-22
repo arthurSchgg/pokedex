31e0d5b179cfef6a22017c791814d430ec849465
arthurSchgg <arthurms2904@gmail.com>
2026-09-22 19:39:22 -0300

    feat(model): cria entidade Pokemon com atributos base

--- src/main/java/com/br/pokedex/model/Pokemon.java
+++ src/main/java/com/br/pokedex/model/Pokemon.java
@@ -1,4 +1,32 @@
 package com.br.pokedex.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
 public class Pokemon {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String tipo1;
+    private String tipo2;
+
+    private int hp;
+    private int ataque;
+    private int defesa;
+    private int velocidade;
+
+    private String imagemUrl;
 }
